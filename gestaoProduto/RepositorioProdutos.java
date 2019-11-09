cf0ef9395a8abc2486c97ac88033ca18f859a955
Carlos Pereira <56885446+chenrique13@users.noreply.github.com>
2019-11-09 16:06:29 -0300

    Update RepositorioProdutos.java

--- gestaoProduto/RepositorioProdutos.java
+++ gestaoProduto/RepositorioProdutos.java
@@ -5,26 +5,18 @@ import java.util.List;
 public interface RepositorioProdutos {
 
 	void inserir(Livro a);
-
 	void inserir(Revista r);
-
 	void inserir(Jornal j);
-
+	
 	void removerL(int numLivro);
-
 	void removerR(int numRevista);
-
 	void removerJ(int numJornal);
 
 	Livro procurarL(int numLivro);
-
 	Revista procurarR(int numRevista);
-
 	Jornal procurarJ(int numJornal);
 
 	List<Livro> listarLivro();
-
 	List<Revista> listarRevista();
-
 	List<Jornal> listarJornal();
 }
